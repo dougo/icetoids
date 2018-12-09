41190e6fe56927edc0c1cb7822fbe69408a8b9dd
Doug Orleans <doug@wellist.com>
2018-12-09 18:05:49 -0500

    Add a title.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <h1>IceToids</h1>
+      </div>
+    );
   }
 }
--- test/components/AppSpec.js
+++ test/components/AppSpec.js
@@ -6,6 +6,7 @@ import App from 'components/App';
 describe('App', () => {
   it('renders a div', () => {
     const subject = mount(<App />);
-    expect(subject).to.have.tagName('div');
+    expect(subject).to.have.descendants('h1');
+    expect(subject.find('h1')).to.have.text('IceToids');
   });
 });
